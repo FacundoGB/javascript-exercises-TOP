93c7588d1a899f86793a721a7e5c790ed918187c
facundogb <bardifacundo@gmail.com>
2022-12-22 20:47:46 -0300

    solved exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,34 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    
+    //lower case and remove punctuation & spaces
+    let noPunctuation = string.replace(/\W/g,"");
+    let noSpaces = noPunctuation.replace(/\s/g,"").toLowerCase();
+
+    //loop
+    for (let i = 0; i < noSpaces.length/2; i++) {
+        console.log("String position i: " + noSpaces[i]);
+        console.log("String position length-1-i: " + noSpaces[noSpaces.length -1 -i]);
+        if (noSpaces[i] !== noSpaces[noSpaces.length -1 -i]) {
+            return false;
+        }
+    }
+    return true;
+
+
+
+
+/**
+ * This doesn't work. It checks string[i] against every string[j]
+ */
+    // for(let i =0; i< noSpaces.length; i++){
+    //     for (let j = noSpaces.length-1; j>=0; j--) {
+    //         if(noSpaces.charAt(i).toLowerCase() !== noSpaces.charAt(j).toLowerCase()) {
+    //             return false;
+    //         } 
+
+    //     } 
+    // }
+
 
 };
 
