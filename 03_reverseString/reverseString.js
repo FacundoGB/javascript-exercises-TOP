a3518895e0414396891b2d5d422acb6f0013ab4b
facundogb <bardifacundo@gmail.com>
2022-12-22 13:05:08 -0300

    completed exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(s) {
+    let stringArray = s.split(""); // ["h", "e", "l", "l", "o"]
 
+    // reverse the new created array
+    let reverseArray = stringArray.reverse(); // ["h", "e", "l", "l", "o"].reverse();
+
+    // join all elements of the array into a string
+    let joinArray = reverseArray.join(""); // ["o", "l", "l", "e", "h"].join("")
+
+    return joinArray;
 };
 
 // Do not edit below this line
