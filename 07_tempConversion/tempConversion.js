5f262c09f21fc84105c54cf399868c59143d9ca8
facundogb <bardifacundo@gmail.com>
2022-12-22 16:54:04 -0300

    solved exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  //let conversion = ((f-32)*5) / 9
+  let celcius = Math.round((((f-32)*5) / 9) *10) / 10;
+  return celcius
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  let farenheit = Math.round(((c*1.8)+32) *10) / 10;
+  return farenheit
 };
 
 // Do not edit below this line
