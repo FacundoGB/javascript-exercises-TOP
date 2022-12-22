bc4ec8c4f33d9ba3b913ff663d5ff1cdc3462212
facundogb <bardifacundo@gmail.com>
2022-12-22 12:57:34 -0300

    made function work for all cases

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(s, n) {
+    if (n > 0) {
+        return s.repeat(n);
+    }else if (n === 0 || s === '') {
+        return "";
+    } else if (n < 0) {
+        return "ERROR";
+    }
+    
 };
 
 // Do not edit below this line
