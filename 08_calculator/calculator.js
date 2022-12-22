f396ff8d33442587c531bd33970c649763dabe65
facundogb <bardifacundo@gmail.com>
2022-12-22 18:32:44 -0300

    completed exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+  let values = 0;
+	for (let i = 0; i<a.length; i++) {
+    values = values + a[i];
+  }
+  return values;
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  let mult = 1;
+  for( let i = 0; i<a.length; i++) {
+    mult = mult * a[i];
+  }
+  return mult;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0) {
+    return 1;
+  }
+	return (num * factorial(num -1));
 };
 
 // Do not edit below this line
