57dc63e7e207129129ea4f3f9c57ef180d34e774
facundogb <bardifacundo@gmail.com>
2022-12-22 23:11:50 -0300

    completed exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    
+    let n1 = 0, n2 = 1, temp;
+    if (n<0) {
+        console.log("OOPS");
+        return "OOPS";
+    }
+    for(let i = 1; i<=n; i++) {
+        temp = n1 + n2;
+        console.log("temporal: " + temp);
+        n1 = n2;
+        console.log("n1: " + n1);
+        n2 = temp;
+    }
+    return n1
+
+    // for (let i = 0; i <= n; i++) {
+    //     fibo = fibo + i;
+    // }
+    // return fibo;
 
 };
 
